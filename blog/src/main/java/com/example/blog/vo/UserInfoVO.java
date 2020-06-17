a01b313fadc3023742a0f1233f4128236d163cc7
hourui <2579023575@qq.com>
2020-06-17 09:53:01 +0800

    feat: by hourui

--- blog/src/main/java/com/example/blog/vo/UserInfoVO.java
+++ blog/src/main/java/com/example/blog/vo/UserInfoVO.java
@@ -1,31 +1,12 @@
 package com.example.blog.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserInfoVO {
     private Integer userID;
-    private String userName;
-    private String userimg;
-
-    public Integer getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Integer userID) {
-        this.userID = userID;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserimg() {
-        return userimg;
-    }
-
-    public void setUserimg(String userimg) {
-        this.userimg = userimg;
-    }
+    private String username;
+    private String userImg;
 }
