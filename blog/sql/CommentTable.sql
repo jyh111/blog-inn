6b1a3681bd7b62e773f561de9150fc6a8e77d389
hourui <2579023575@qq.com>
2020-06-27 19:12:44 +0800

    feat: sql source data

---
+++ blog/sql/CommentTable.sql
@@ -0,0 +1,15 @@
+INSERT INTO `Comment` VALUES (1,10,'你的文章我好喜欢，放入我的收藏夹喜欢  1  了',1,2);
+INSERT INTO `Comment` VALUES（2,12,'你的文章我好喜欢，放入我的收藏夹喜欢  2  了',1,2);
+INSERT INTO `Comment` VALUES（3,14,'你的文章我好喜欢，放入我的收藏夹喜欢  3  了',1,2);
+INSERT INTO `Comment` VALUES（4,19,'你的文章我好喜欢，放入我的收藏夹喜欢  4  了',2,3);
+INSERT INTO `Comment` VALUES（5,21,'你的文章我好喜欢，放入我的收藏夹喜欢  5  了',2,3);
+INSERT INTO `Comment` VALUES（6,23,'你的文章我好喜欢，放入我的收藏夹喜欢  6  了',2,3);
+INSERT INTO `Comment` VALUES（7,28,'你的文章我好喜欢，放入我的收藏夹喜欢  7  了',3,4);
+INSERT INTO `Comment` VALUES（8,30,'你的文章我好喜欢，放入我的收藏夹喜欢  8  了',3,4);
+INSERT INTO `Comment` VALUES（9,32,'你的文章我好喜欢，放入我的收藏夹喜欢  9  了',3,4);
+INSERT INTO `Comment` VALUES（10,37,'你的文章我好喜欢，放入我的收藏夹喜欢  10  了',4,5);
+INSERT INTO `Comment` VALUES（11,39,'你的文章我好喜欢，放入我的收藏夹喜欢  11  了',4,5);
+INSERT INTO `Comment` VALUES（12,41,'你的文章我好喜欢，放入我的收藏夹喜欢  12  了',4,5);
+INSERT INTO `Comment` VALUES（13,1,'你的文章我好喜欢，放入我的收藏夹喜欢  13  了',5,1);
+INSERT INTO `Comment` VALUES（14,3,'你的文章我好喜欢，放入我的收藏夹喜欢  14  了',5,1);
+INSERT INTO `Comment` VALUES（15,5,'你的文章我好喜欢，放入我的收藏夹喜欢  15  了',5,1);
