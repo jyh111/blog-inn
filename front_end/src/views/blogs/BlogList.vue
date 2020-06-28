ddf17805779ccb0c4f36988d6f3111eb356672f6
jyh111 <jlmctsb@126.com>
2020-06-28 15:31:54 +0800

    page_view

--- front_end/src/views/blogs/BlogList.vue
+++ front_end/src/views/blogs/BlogList.vue
@@ -2,16 +2,16 @@
 <template>
 	<div>
 		<Header></Header>
-
+		<div class="content">
 		<a-list item-layout="horizontal" :data-source="blogList" >
 			<a-list-item slot="renderItem" slot-scope="item, index">
 				<router-link :to="{name:'DisplayBlog',query:{blogId:item.blogId}}">
 				<span>{{item.title}}</span>
-				<span>{{item.page_view}}</span>
 				<a-icon v-if="userInfo.userId!=0" type="heart" :theme="item.infavor=='null'?'twoTone':'filled'"></a-icon>
 				</router-link>
 			</a-list-item>
 		</a-list>
+		</div>
 	</div>
 </template>
 
@@ -29,6 +29,10 @@ export default{
 	},
 	created() {
 		this.set_userInfo(sessionStorage.getItem('userInfo'))
+
+	},
+	mounted() {
+		console.log(this.blogList)
 	},
 	computed:{
 		...mapGetters([
@@ -48,6 +52,10 @@ export default{
 </script>
 
 <style>
-
+.content{
+	background-color: #FFFFFF;
+	width: 80%;
+	height: 100%;
+}
 
 </style>
