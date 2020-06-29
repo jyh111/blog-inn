bbb5ac414bf18f61e9d7df53af11478626cf0795
hourui <2579023575@qq.com>
2020-06-29 11:43:49 +0800

    fix: xxx

--- blog/src/main/java/com/example/blog/blImpl/MessageServiceImpl.java
+++ blog/src/main/java/com/example/blog/blImpl/MessageServiceImpl.java
@@ -25,8 +25,7 @@ public class MessageServiceImpl implements MessageService {
 	@Override
 	public ResponseVO addMessage(MessageVO messageVO) {
 		Message message = new Message();
-//		message.setMessageId(messageVO.getMessageId());
-		message.setCommentId(messageVO.getCommentId());
+//		message.setMessageId(messageVO.getMessageId())
 		message.setSender(messageVO.getSender().getUserId());
 		message.setRecipient(messageVO.getRecipient().getUserId());
 		message.setBlogId(messageVO.getBlogId());
